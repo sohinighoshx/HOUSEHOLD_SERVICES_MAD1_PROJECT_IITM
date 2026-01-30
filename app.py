1f39d2595966489df26767eaad24c3ca93748cb2
SOHINI GHOSH <sohinighosh1655@gmail.com>
2026-01-30 19:31:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template, request, redirect, url_for,session
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+import config
+import models
+import routes
