7f0873a6cf47722d6a178e3a8a17ae02bb23c2ad
SOHINI GHOSH <sohinighosh1655@gmail.com>
2026-01-30 19:34:54 +0530

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for,session
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-import config
-import models
-import routes
