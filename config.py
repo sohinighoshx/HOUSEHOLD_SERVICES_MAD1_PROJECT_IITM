88458d1a9ee2d3b8d8a53fa1b4e6b55db599d0fa
SOHINI GHOSH <sohinighosh1655@gmail.com>
2026-01-30 19:35:03 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-from os import getenv
-from app import app
-load_dotenv()
-
-app.config['SQLALCHEMY_DATABASE_URI']=getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
-
-print('Configured!!')
